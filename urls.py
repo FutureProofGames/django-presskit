7cfcfb8d36189b2e7d758da00ab35466f746899d
Gregory Avery-Weir <Gregory.Weir@gmail.com>
2018-03-24 19:30:56 -0400

    dpk-4: Implement company page

--- urls.py
+++ urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.presskit, name='default'),
+    url(r'^(?P<company_id>[0-9]+)/$', views.presskit, name='company'),
+    url(r'^projects/(?P<company_id>[0-9]+)/$', views.project, name='project'),
 ]
