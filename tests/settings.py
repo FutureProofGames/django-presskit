45fe082b94f6979488cb912d6cc903a0bf2c43ac
Gregory Avery-Weir <Gregory.Weir@gmail.com>
2020-12-20 15:51:00 -0500

    DPK-51: Make initial changes for Django 2 and 3

--- tests/settings.py
+++ tests/settings.py
@@ -3,11 +3,13 @@ INSTALLED_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sites",
+    "django.contrib.messages",
     "tests",
     "django_presskit",
     "easy_thumbnails",
     "filer",
     "adminsortable2",
+    "django.contrib.admin",
 ]
 
 DATABASES = {
@@ -19,12 +21,27 @@ DATABASES = {
 
 ROOT_URLCONF = "tests.urls"
 
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.request',
+                'django.contrib.messages.context_processors.messages',
+            ]
         },
     },
 ]
